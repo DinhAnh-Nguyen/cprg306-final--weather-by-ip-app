8bb23e76fa8739b3e9fc5203f360387c2287eafd
bnhao <88442790+bnhao@users.noreply.github.com>
2024-12-09 19:25:21 -0700

    update again

--- app/utilities/weather-stack-api.js
+++ app/utilities/weather-stack-api.js
@@ -1,8 +1,10 @@
-const WEATHERSTACK_API_KEY = "";
+
 
 export const fetchWeatherByIP = async (ip) => {
+    const weather_api_key = process.env.WEATHERSTACK_API_KEY;
+
     const response = await fetch(
-        `http://api.weatherstack.com/current?access_key=${WEATHERSTACK_API_KEY}&query=${ip}`
+        `http://api.weatherstack.com/current?access_key=${"e259eda57f48d2f35952797af8c65e52"}&query=${ip}`
     );
     const data = await response.json();
 
--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,9 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+    env: {
+        WEATHERSTACK_API_KEY: process.env.WEATHERSTACK_API_KEY,
+    },
+};
