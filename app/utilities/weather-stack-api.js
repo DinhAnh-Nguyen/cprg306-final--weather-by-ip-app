1d238d4ee9c9ec340c91a9c32098bd823d2d3956
bnhao <88442790+bnhao@users.noreply.github.com>
2024-12-09 19:29:20 -0700

    delete api key

--- app/utilities/weather-stack-api.js
+++ app/utilities/weather-stack-api.js
@@ -4,7 +4,7 @@ export const fetchWeatherByIP = async (ip) => {
     const weather_api_key = process.env.WEATHERSTACK_API_KEY;
 
     const response = await fetch(
-        `http://api.weatherstack.com/current?access_key=${"e259eda57f48d2f35952797af8c65e52"}&query=${ip}`
+        `http://api.weatherstack.com/current?access_key=${""}&query=${ip}`
     );
     const data = await response.json();
 
